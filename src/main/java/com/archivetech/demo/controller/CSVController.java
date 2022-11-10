6a2daca4a42a1319a1a03654177e6b0e057849d7
maxwell1700 <43969904+maxwell1700@users.noreply.github.com>
2022-11-10 17:02:14 +0000

    updated service implementation

--- src/main/java/com/archivetech/demo/controller/CSVController.java
+++ src/main/java/com/archivetech/demo/controller/CSVController.java
@@ -2,12 +2,10 @@ package com.archivetech.demo.controller;
 
 import com.archivetech.demo.service.UpdateCSVService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/csv")
@@ -15,11 +13,16 @@ public class CSVController {
     @Autowired
      private UpdateCSVService updateCSVService;
     // set a string that will take the file path
-    String path = "C:\\Users\\maxwe\\Downloads\\ac56fe111.csv";
 
-    @GetMapping("/getfile")
-    public void getCSVFile() throws IOException {
-     updateCSVService.readCSVFile(path);
+
+    @GetMapping("/getfile/{name}")
+    public String getCSVRecordByName(@PathVariable String name) throws IOException {
+        return updateCSVService.readFileByName(name);
+    }
+
+    @GetMapping("/files")
+    public List<String> getCSVFile() throws IOException {
+       return  updateCSVService.readAllCSVFile();
     }
 
     @PostMapping("/{id}")
--- src/main/java/com/archivetech/demo/service/UpdateCSVService.java
+++ src/main/java/com/archivetech/demo/service/UpdateCSVService.java
@@ -1,24 +1,40 @@
 package com.archivetech.demo.service;
 
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.stereotype.Service;
+
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+// create a list<String> users = path .get path. collect collectors.tolist
 
 public class UpdateCSVService {
+    String path = "C:/Users/maxwe/Downloads/ac56fe111.csv";
+    List<String> files = Files.lines(Paths.get(path)).
+            collect(Collectors.toList());
 
-    public void readCSVFile(String path) throws IOException {
-// provide general template on how to read a csv file
-         Files.lines(Paths.get(path))
-                .skip(1)
-                .forEach(System.out::println);
+
+    public UpdateCSVService() throws IOException {
     }
 
+    public List<String> readAllCSVFile() throws IOException {
+// provide general template on how to read a csv file
+         return files;
+    }
+
+    public String readFileByName(String name){
+        return files.stream().filter(file -> file.contains(name)).toList().toString();
+
+    }
     public void writeCSVFile(){
 // provide general template on how to read a csv file
     }
+
+    //method to sort by a specific column
 }
 
